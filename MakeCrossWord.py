e7692bd3aa2a476e97a40a9067bce243a656a5a9
afoster1029 <alexanderpfoster@gmail.com>
2018-04-12 15:35:18 -0500

    code for creating the graph

---
+++ MakeCrossWord.py
@@ -0,0 +1,16 @@
+import csv
+
+
+"""
+The plan is:
+1. Read from the CSV file and choose a random word.
+2. Build the crossword with that word.
+3. Select the next words that work best for completing the cross word. 
+4. Continue 3 and 4 until we have the crossword. 
+"""
+
+with open('dictFile.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ')
+    for row in spamreader:
+        print(row)
+
